8cc290ce1913d929f5f293f6d309f26c50cb3a9f
Eugene Gaitano <dev.gaitano@gmail.com>
2026-04-17 09:29:50 +0300

    Implement post fetching and display functionality

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your code here!
+// Display posts
+function displayPosts(posts) {
+  // Get post list el
+  const postList = document.getElementById("post-list")
+
+  // Create li with h1 & p for each post
+  posts.forEach(post => {
+    const li = document.createElement("li")
+
+    const h1 = document.createElement("h1")
+    h1.textContent = post.title
+
+    const p = document.createElement("p")
+    p.textContent = post.body
+
+    // Append el
+    li.appendChild(h1)
+    li.appendChild(p)
+    postList.appendChild(li)
+  });
+}
+
+// Fetch posts
+async function fetchPosts() {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts")
+    const posts = await res.json()
+
+    displayPosts(posts)
+  } catch (e) {
+    console.log("Error fetching posts: ", e)
+
+  }
+}
+
+// Call fn
+fetchPosts()
